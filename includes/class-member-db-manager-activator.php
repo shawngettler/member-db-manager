11e69e73ac078ceff9d04c3fe772d876b6df89d6
Shawn Gettler <shawn.gettler@gmail.com>
2021-03-02 01:42:04 +0000

    added table creation on activation

--- includes/class-member-db-manager-activator.php
+++ includes/class-member-db-manager-activator.php
@@ -5,10 +5,47 @@
  */
 class Member_DB_Manager_Activator {
 
+    // defaults
+    public static $default_options = array(
+        'version' => MEMBER_DB_MANAGER_VERSION,
+        'db_name' => 'Member List'
+    );
+    public static $default_schema = array(
+        'id SMALLINT UNSIGNED AUTO_INCREMENT PRIMARY KEY',
+        'firstname VARCHAR(32) NOT NULL',
+        'lastname VARCHAR(32) NOT NULL',
+        'email VARCHAR(64) NOT NULL'
+    );
+
     /**
-     * Activate the plugin.
+     * Activate the plugin. Check for existing options and tables and try
+     * to upgrade them to the current plugin version.
      */
     public static function activate() {
+
+        // check options
+        $option_name = 'member_db_manager_option';
+        $plugin_options = get_option($option_name);
+        if(!$plugin_options) {
+            $plugin_options = Member_DB_Manager_Activator::$default_options;
+            add_option($option_name, $plugin_options);
+        } else {
+            error_log('Plugin option '.$option_name.' aleady exists.');
+            // upgrade from old plugin version as required
+        }
+
+        // check tables
+        global $wpdb;
+        $table_name = $wpdb->prefix.'member_db_manager_data';
+        if($wpdb->get_var($wpdb->prepare('SHOW TABLES LIKE %s', $table_name)) != $table_name) {
+            $table_schema = implode(', ', Member_DB_Manager_Activator::$default_schema);
+            $table_charset = $wpdb->get_charset_collate();
+            $wpdb->query('CREATE TABLE '.$table_name.' ( '.$table_schema.' ) '.$table_charset);
+
+        } else {
+            error_log('Plugin table '.$table_name.' already exists.');
+            // upgrade from old plugin version as required
+        }
     }
 
 }
