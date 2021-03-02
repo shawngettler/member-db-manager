82c14954326deb2d1b63d4ccfae06d6cedcf0d16
Shawn Gettler <shawn.gettler@gmail.com>
2021-03-02 12:46:17 +0000

    updated default table schema

--- includes/class-member-db-manager-activator.php
+++ includes/class-member-db-manager-activator.php
@@ -12,10 +12,25 @@ class Member_DB_Manager_Activator {
     );
     public static $default_schema = array(
         'id SMALLINT UNSIGNED AUTO_INCREMENT PRIMARY KEY',
+        'hid SMALLINT UNSIGNED',
+        'membertype TINYINT UNSIGNED NOT NULL',
+        'memberterm TINYINT UNSIGNED NOT NULL',
+        'createdate DATE NOT NULL',
+        'updatedate DATE NOT NULL',
+        'expiredate DATE NOT NULL',
         'firstname VARCHAR(32) NOT NULL',
         'lastname VARCHAR(32) NOT NULL',
-        'email VARCHAR(64) NOT NULL'
+        'email VARCHAR(64) NOT NULL',
+        'street VARCHAR(128) NOT NULL',
+        'city VARCHAR(32) NOT NULL',
+        'province CHAR(2) NOT NULL',
+        'country VARCHAR(32) NOT NULL',
+        'postalcode CHAR(6) NOT NULL',
+        'phone CHAR(10)',
+        'message VARCHAR(256)',
+        'hearabout TINYINT'
     );
+
 
     /**
      * Activate the plugin. Check for existing options and tables and try
@@ -29,6 +44,7 @@ class Member_DB_Manager_Activator {
         if(!$plugin_options) {
             $plugin_options = Member_DB_Manager_Activator::$default_options;
             add_option($option_name, $plugin_options);
+
         } else {
             error_log('Plugin option '.$option_name.' aleady exists.');
             // upgrade from old plugin version as required
@@ -37,7 +53,7 @@ class Member_DB_Manager_Activator {
         // check tables
         global $wpdb;
         $table_name = $wpdb->prefix.'member_db_manager_data';
-        if($wpdb->get_var($wpdb->prepare('SHOW TABLES LIKE %s', $table_name)) != $table_name) {
+        if($wpdb->get_var('SHOW TABLES LIKE \''.$table_name.'\'') != $table_name) {
             $table_schema = implode(', ', Member_DB_Manager_Activator::$default_schema);
             $table_charset = $wpdb->get_charset_collate();
             $wpdb->query('CREATE TABLE '.$table_name.' ( '.$table_schema.' ) '.$table_charset);
